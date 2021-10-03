bb8b12ce64cab1f8bcc76465197bd8943b0f05b9
mugutdinov <mugutdinov@protei.ru>
2021-10-04 01:39:34 +0300

    work

--- main.cpp
+++ main.cpp
@@ -80,20 +80,14 @@ public:
 
     [[nodiscard("reason")]] bool from_string(const std::string &ip_str) //change to string_view
     {
-            /*for(std::size_t i = 0; i < 4; i++) {
-                if (lhs.b0 != std::stoi(rhs[i]))
-            }*/
-
-//        b0 = std::stoi(i)
-
         auto splitted = split(ip_str, '.');
         if(splitted.size() != 4)
             return false;
 
-        bytes.b0 = std::stoi(splitted[0]);
-        bytes.b1 = std::stoi(splitted[1]);
-        bytes.b2 = std::stoi(splitted[2]);
-        bytes.b3 = std::stoi(splitted[3]);
+        b0 = std::stoi(splitted[0]);
+        b1 = std::stoi(splitted[1]);
+        b2 = std::stoi(splitted[2]);
+        b3 = std::stoi(splitted[3]);
 
         return true;
     }
@@ -109,37 +103,71 @@ public:
 
 public:
     //std::byte
-    struct ip_4bytes{
-        unsigned char b3;
-        unsigned char b2;
-        unsigned char b1;
-        unsigned char b0;
-    };
-    union {
+    //std::variant
+        union {
         uint32_t ip32 = 0;
-        ip_4bytes bytes;
-    };
+            struct {
+                unsigned char b3;
+                unsigned char b2;
+                unsigned char b1;
+                unsigned char b0;
+            };
+        };
 };
 
 std::ostream& operator<<(std::ostream& s, const ip_addr& value)
 {
-    s << static_cast<unsigned>(value.bytes.b0)<<'.'
-      << static_cast<unsigned>(value.bytes.b1)<<'.'
-      << static_cast<unsigned>(value.bytes.b2)<<'.'
-      << static_cast<unsigned>(value.bytes.b3);
+    s << static_cast<unsigned>(value.b0)<<'.'
+      << static_cast<unsigned>(value.b1)<<'.'
+      << static_cast<unsigned>(value.b2)<<'.'
+      << static_cast<unsigned>(value.b3);
     return s;
 }
 
 bool operator<(const ip_addr& lhs, const ip_addr& rhs)
 {
-//    for(std::size_t i = 0; i < 4; i++)
-//    {
-//        if(lhs.b0 != std::stoi(rhs[i]))
-//            return std::stoi(lhs[i]) > std::stoi(rhs[i]);
-//    }
     return lhs.ip32 < rhs.ip32;
 }
 
+bool operator>(const ip_addr& lhs, const ip_addr& rhs)
+{
+    return lhs.ip32 > rhs.ip32;
+}
+
+std::vector<ip_addr> filter(const std::vector<ip_addr>& ips, const std::function<bool(ip_addr)>& predicat)
+{
+    std::vector<ip_addr> filtered_ip;
+    for(auto&& ip : ips)
+    {
+        if(predicat(ip))
+        {
+            filtered_ip.push_back(ip);
+        }
+    }
+    return filtered_ip;
+}
+
+
+std::vector<ip_addr> filter(const std::vector<ip_addr>& ips, unsigned char first)
+{
+    return filter(ips, [first](const ip_addr& ip){
+        return ip.b0 == first;
+    });
+}
+
+std::vector<ip_addr> filter(const std::vector<ip_addr>& ips, unsigned char first, unsigned char second)
+{
+    return filter(ips, [first, second](const ip_addr& ip){
+        return (ip.b0 == first && ip.b1 == second);
+    });
+}
+
+std::vector<ip_addr> filter_any(const std::vector<ip_addr>& ips, unsigned char byte)
+{
+    return filter(ips, [byte](const ip_addr& ip){
+        return (ip.b0 == byte || ip.b1 == byte || ip.b2 == byte || ip.b3 == byte);
+    });
+}
 
 int main(int argc, char const *argv[])
 {
@@ -162,74 +190,37 @@ int main(int argc, char const *argv[])
             ip_pool.push_back(ip);
         }
 
-        std::sort(std::begin(ip_pool), std::end(ip_pool));
+        std::sort(std::begin(ip_pool), std::end(ip_pool), std::greater<>());
         for(auto ip : ip_pool)
         {
             std::cout << ip<<std::endl;
         }
 
-        // 222.173.235.246
-        // 222.130.177.64
-        // 222.82.198.61
-        // ...
-        // 1.70.44.170
-        // 1.29.168.152
-        // 1.1.234.8
+        auto filtered_by_first = filter(ip_pool, 1);
+        std::sort(std::begin(filtered_by_first), std::end(filtered_by_first), std::greater<>());
 
-        // TODO filter by first byte and output
-        // ip = filter(1)
+        for(auto ip : filtered_by_first)
+        {
+            std::cout << ip<<std::endl;
+        }
 
-        // 1.231.69.33
-        // 1.87.203.225
-        // 1.70.44.170
-        // 1.29.168.152
-        // 1.1.234.8
 
-        // TODO filter by first and second bytes and output
-        // ip = filter(46, 70)
+        auto filtered_by_first_second = filter(ip_pool, 46, 70);
+        std::sort(std::begin(filtered_by_first_second), std::end(filtered_by_first_second), std::greater<>());
 
-        // 46.70.225.39
-        // 46.70.147.26
-        // 46.70.113.73
-        // 46.70.29.76
+        for(auto ip : filtered_by_first_second)
+        {
+            std::cout << ip<<std::endl;
+        }
 
-        // TODO filter by any byte and output
-        // ip = filter_any(46)
+        auto filtered_any = filter_any(ip_pool, 46);
+        std::sort(std::begin(filtered_any), std::end(filtered_any), std::greater<>());
 
-        // 186.204.34.46
-        // 186.46.222.194
-        // 185.46.87.231
-        // 185.46.86.132
-        // 185.46.86.131
-        // 185.46.86.131
-        // 185.46.86.22
-        // 185.46.85.204
-        // 185.46.85.78
-        // 68.46.218.208
-        // 46.251.197.23
-        // 46.223.254.56
-        // 46.223.254.56
-        // 46.182.19.219
-        // 46.161.63.66
-        // 46.161.61.51
-        // 46.161.60.92
-        // 46.161.60.35
-        // 46.161.58.202
-        // 46.161.56.241
-        // 46.161.56.203
-        // 46.161.56.174
-        // 46.161.56.106
-        // 46.161.56.106
-        // 46.101.163.119
-        // 46.101.127.145
-        // 46.70.225.39
-        // 46.70.147.26
-        // 46.70.113.73
-        // 46.70.29.76
-        // 46.55.46.98
-        // 46.49.43.85
-        // 39.46.86.85
-        // 5.189.203.46
+        for(auto ip : filtered_any)
+        {
+            std::cout << ip<<std::endl;
+        }
+
     }
     catch(const std::exception &e)
     {
